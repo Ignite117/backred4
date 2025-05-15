3bd57050cdcc59328e0179482fff90a917519c4f
Ignite117 <va.ignatev@mail.ru>
2025-05-15 08:51:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<?php
+
+$errors = [];
+$oldValues = [];
+$savedValues = [];
+
+if (isset($_COOKIE['form_errors'])) {
+    $errors = json_decode($_COOKIE['form_errors'], true);
+    $oldValues = json_decode($_COOKIE['old_values'], true);
+}
+
+
+foreach ($_COOKIE as $name => $value) {
+    if (strpos($name, 'saved_') === 0) {
+        $field = substr($name, 6);
+        $savedValues[$field] = $value;
+    }
+}
+
+
+function getFieldValue($field, $default = '') {
+    global $oldValues, $savedValues;
+
+    if (isset($oldValues[$field])) {
+        return $oldValues[$field];
+    }
+
+    if (isset($savedValues[$field])) {
+        return $savedValues[$field];
+    }
+
+    return $default;
+}
+
+function isSelected($field, $value) {
+    global $oldValues, $savedValues;
+
+    $currentValues = [];
+    if (isset($oldValues[$field])) {
+        if ($field === 'languages') {
+            $currentValues = explode(',', $oldValues[$field]);
+        } else {
+            return $oldValues[$field] === $value ? 'checked' : '';
+        }
+    } elseif (isset($savedValues[$field])) {
+        if ($field === 'languages') {
+            $currentValues = explode(',', $savedValues[$field]);
+        } else {
+            return $savedValues[$field] === $value ? 'checked' : '';
+        }
+    }
+
+    return in_array($value, $currentValues) ? 'selected' : '';
+}
+
+function isChecked($field) {
+    global $oldValues, $savedValues;
+
+    if (isset($oldValues[$field])) {
+        return $oldValues[$field] ? 'checked' : '';
+    }
+
+    if (isset($savedValues[$field])) {
+        return $savedValues[$field] ? 'checked' : '';
+    }
+
+    return '';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ru-RU">
+
+  <head>
+    <title>index</title>
+  </head>
+
+  <body>
+    <?php if (isset($_GET['success'])): ?>
+                <div class="success-message">Данные успешно сохранены!</div>
+            <?php endif; ?>
+
+            <?php if (!empty($errors)): ?>
+                <div class="error-list">
+                    <h3>Обнаружены ошибки:</h3>
+                    <ul>
+                        <?php foreach ($errors as $error): ?>
+                            <li><?php echo htmlspecialchars($error); ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+    <form action="sub.php" method="POST" id="form" class="w-50 mx-auto">
+
+          <label class="form-label">
+             ФИО:<br>
+            <input type="text" class="form-control" placeholder="Введите ваше ФИО" name="name" id = "name" required
+                           value="<?php echo htmlspecialchars(getFieldValue('name')); ?>"
+                           class="<?php echo isset($errors['name']) ? 'error-field' : ''; ?>">
+                    <?php if (isset($errors['name'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['name']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+          <label class="form-label">
+             Телефон:<br>
+            <input class="form-control" type="tel" placeholder="+123456-78-90" name="phone" id="phone" required
+                           value="<?php echo htmlspecialchars(getFieldValue('phone')); ?>"
+                           class="<?php echo isset($errors['phone']) ? 'error-field' : ''; ?>">
+                    <?php if (isset($errors['phone'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['phone']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+          <label class="form-label">
+            e-mail:<br>
+            <input class="form-control" type="email" placeholder="Введите вашу почту" name="email" id="email" required
+                           value="<?php echo htmlspecialchars(getFieldValue('email')); ?>"
+                           class="<?php echo isset($errors['email']) ? 'error-field' : ''; ?>">
+                    <?php if (isset($errors['email'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['email']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+          <label class="form-label">
+             Дата рождения:<br>
+            <input class="form-control" value="2000-07-15" type="date" name="birthdate" id="birthdate" required
+                           value="<?php echo htmlspecialchars(getFieldValue('birthdate')); ?>"
+                           class="<?php echo isset($errors['birthdate']) ? 'error-field' : ''; ?>">
+                    <?php if (isset($errors['birthdate'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['birthdate']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+          <div><br>
+            Пол:<br>
+          <label class="form-check-label"><input type="radio" checked="checked" class="form-check-input" value="male" id="male" name="gender" required
+                               <?php echo isSelected('gender', 'male'); ?>
+                               class="<?php echo isset($errors['gender']) ? 'error-field' : ''; ?>">>
+            Мужской</label>
+          <label class="form-check-label"><input type="radio" class="form-check-input" value="female" id="female" name="gender"
+                               <?php echo isSelected('gender', 'female'); ?>
+                               class="<?php echo isset($errors['gender']) ? 'error-field' : ''; ?>">>
+            Женский</label><br>
+            <?php if (isset($errors['gender'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['gender']); ?></div>
+                    <?php endif; ?>
+
+          </div><br>
+
+          <label class="form-label">
+             Любимый язык программирования:<br>
+            <select class="form-select" id="languages" name="languages[]" multiple="multiple" required class="<?php echo isset($errors['languages']) ? 'error-field' : ''; ?>" size="5">
+                        <?php
+                        $allLanguages = ['Pascal', 'C', 'C++', 'JavaScript', 'PHP', 'Python', 'Java', 'Haskell', 'Clojure', 'Prolog', 'Scala'];
+                        foreach ($allLanguages as $lang): ?>
+                            <option value="<?php echo htmlspecialchars($lang); ?>"
+                                <?php echo isSelected('languages', $lang); ?>>
+                                <?php echo htmlspecialchars($lang); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                    <?php if (isset($errors['languages'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['languages']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+          <label class="form-label">
+             Биография:<br>
+            <input type="text" class="form-control" id="bio" name="bio" required
+                              class="<?php echo isset($errors['bio']) ? 'error-field' : ''; ?>"><?php
+                              echo htmlspecialchars(getFieldValue('bio')); ?></textarea>
+                    <?php if (isset($errors['bio'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['bio']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+            :<br>
+          <label class="form-check-label"><input type="checkbox" class="form-check-input" name="agreement" id="agreement" value="1" required
+                           <?php echo isChecked('agreement'); ?>
+                           class="<?php echo isset($errors['agreement']) ? 'error-field' : ''; ?>">
+                    С условиями ознакомлен(а)
+                    <?php if (isset($errors['agreement'])): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($errors['agreement']); ?></div>
+                    <?php endif; ?>
+          </label><br>
+
+            Сохранить:<br>
+          <button type="submit" name="save" class="btn btn-primary">Опубликовать</button>
+
+    </form>
+  </body>
+
+</html>
